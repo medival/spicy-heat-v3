165146d1ed4f40e44f8c3b4e856c0d1963b2c0a4
medival <dev.adipurnomo@gmail.com>
2021-07-05 17:13:35 +0700

    update: Web Improvement

--- src/scripts/global/config.js
+++ src/scripts/global/config.js
@@ -1,6 +1,6 @@
 const CONFIG = {
   BASE_URL: 'https://restaurant-api.dicoding.dev/',
-  BASE_IMAGE_URL: 'https://restaurant-api.dicoding.dev/images/medium/',
+  BASE_IMAGE_URL: 'https://restaurant-api.dicoding.dev/images/small/',
   DEFAULT_LANGUAGE: 'en-us',
   CACHE_NAME: new Date().toISOString(),
   DATABASE_NAME: 'spicy-heat-database',
