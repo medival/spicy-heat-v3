603564f630b987354f60b878821088a13efece03
medival <dev.adipurnomo@gmail.com>
2021-07-05 22:48:43 +0700

    update: Fixing typo

--- src/scripts/views/templates/template-creator.js
+++ src/scripts/views/templates/template-creator.js
@@ -63,7 +63,7 @@ const createAvailableDrinksMenu = (drink, dummy) => `
 
 const createCustomerReview = (review) => `
 		<div class="reviewer">
-		  <img class="lazyload" data-src=""https://raw.githubusercontent.com/medival/dummy-data/master/images/avatar/user-profile.webp" alt="avatar">
+		  <img class="lazyload" data-src="https://raw.githubusercontent.com/medival/dummy-data/master/images/avatar/user-profile.webp" alt="avatar">
 		  <div class="reviewer__name">
 		    <p> ${review.name} </p>
 		  </div>
