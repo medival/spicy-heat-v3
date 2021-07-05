30b6fde9d14013be358cfe1187716bab1e86f1f7
medival <dev.adipurnomo@gmail.com>
2021-07-05 22:50:35 +0700

    update: Add config get image medium

--- src/scripts/global/config.js
+++ src/scripts/global/config.js
@@ -1,6 +1,7 @@
 const CONFIG = {
   BASE_URL: 'https://restaurant-api.dicoding.dev/',
   BASE_IMAGE_URL: 'https://restaurant-api.dicoding.dev/images/small/',
+  BASE_IMAGE_URL_MEDIUM: 'https://restaurant-api.dicoding.dev/images/medium/',
   DEFAULT_LANGUAGE: 'en-us',
   CACHE_NAME: new Date().toISOString(),
   DATABASE_NAME: 'spicy-heat-database',
