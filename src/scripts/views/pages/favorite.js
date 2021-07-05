93aa94f7ecf75b2546adc6f4aefcb80e8070af41
medival <dev.adipurnomo@gmail.com>
2021-07-05 10:43:55 +0700

    update: Change resto_empty_label

--- src/scripts/views/pages/favorite.js
+++ src/scripts/views/pages/favorite.js
@@ -7,8 +7,8 @@ const Favorite = {
 			<section class="resto">
 				<div class="resto__container">
 					<h2 class="resto__label"> Your Favorite Restaurant </h2>
+          <h3 class="resto__empty_label" id="empty"> You dont have any favorite restaurant </h3>
 					<div id="resto" class="resto__list">
-            <h2 class="resto__empty_resto"> You don't have any favorite restaurant </h2>
 					</div>
 				</div>
 			</section>
@@ -18,10 +18,15 @@ const Favorite = {
   async afterRender() {
     const restos = await FavoriteRestoIdb.getAllRestos();
     const restosContainer = document.querySelector('#resto');
-    restos.forEach((resto) => {
-      console.log(resto);
-      restosContainer.innerHTML += createRestoItemTemplate(resto);
-    });
+    const emptyLabel = document.querySelector('#empty');
+
+    if (restos.length !== 0) {
+      restos.forEach((resto) => {
+        console.log(resto);
+        restosContainer.innerHTML += createRestoItemTemplate(resto);
+      });
+      emptyLabel.style.display = 'none';
+    }
   },
 
 };
