b1ff8daefadaf0deec80d74505caffa87a6c7d7d
medival <dev.adipurnomo@gmail.com>
2021-07-05 23:35:02 +0700

    update: footBar component

--- src/scripts/components/foot-bar.js
+++ src/scripts/components/foot-bar.js
@@ -6,10 +6,21 @@ class footBar extends HTMLElement {
   render() {
     this.innerHTML = `
 		<footer>
-			<div class="footer__content">
-				<p> Crafted by me to everyone with spirit!</p>
-				<p class="footer__copyright"> Copyright &copy; 2021 - Spicy Heat </p>
-			</div>
+			 <div class="footer__content">
+         <p> Crafted by me to everyone with spirit!</p>
+         <p class="footer__copyright"> Copyright &copy; 2021 - Spicy Heat </p>
+         <div class="footer__social-media-icons">
+            <a href="#" class="social-icons social-icon-twitter">
+              <i class="fa fa-lg fa-twitter"></i>
+            </a>
+            <a href="#" class="social-icons social-icon-linkedin">
+              <i class="fa fa-lg fa-linkedin"></i>
+            </a>
+            <a href="https://github.com/medival"  target="_blank" class="social-icons social-icon-github">
+              <i class="fa fa-lg fa-github"></i>
+            </a>
+         </div>
+        </div>
 		</footer>
 		`;
   }
