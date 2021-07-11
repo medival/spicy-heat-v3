96452b85d1c37ecca12df0214a703754a497ea7b
medival <dev.adipurnomo@gmail.com>
2021-07-11 23:04:14 +0700

    update: Change cache_name

--- src/scripts/global/config.js
+++ src/scripts/global/config.js
@@ -3,7 +3,7 @@ const CONFIG = {
   BASE_IMAGE_URL: 'https://restaurant-api.dicoding.dev/images/small/',
   BASE_IMAGE_URL_MEDIUM: 'https://restaurant-api.dicoding.dev/images/medium/',
   DEFAULT_LANGUAGE: 'en-us',
-  CACHE_NAME: new Date().toISOString(),
+  CACHE_NAME: 'spicy-heat',
   DATABASE_NAME: 'spicy-heat-database',
   DATABASE_VERSION: 1,
   OBJECT_STORE_NAME: 'resto',
